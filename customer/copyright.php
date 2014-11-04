4ae78b5b51856c9a195e66874027b3044c962e4e
Alice <alicehang626@gmail.com>
2014-11-03 21:25:53 -0800

    delete
    all

--- customer/copyright.php
+++
@@ -1,5 +0,0 @@
-<div id="copy-right">
-		<label>
-			@ Xiaoya Hang All rights reserved. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;Privacy Policy&nbsp;&nbsp; &nbsp;| &nbsp;&nbsp;&nbsp;Terms + Conditions
-		</label>
-	</div>
